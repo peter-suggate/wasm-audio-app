cc119fb6a61788ffc276e5e60340ae9a0b8ade23
Peter Suggate <petersuggate@gmail.com>
2020-11-30 14:30:25 +1300

    Improve performance and comments for the audio processor process() function.

--- public/AudioProcessor.js
+++ public/AudioProcessor.js
@@ -5,38 +5,87 @@ class AudioProcessor extends AudioWorkletProcessor {
   constructor() {
     super();
 
-    this.samples = [];
+    // Initialized to an array holding a buffer of samples for analysis later -
+    // once we know how many samples need to be stored.
+    this.samples = undefined;
+    this.totalSamples = 0;
 
+    // Listen to events from the AudioNode running on the main thread.
     this.port.onmessage = (event) => this.onmessage(event.data);
 
     this.detector = null;
   }
 
-  onmessage = (eventData) => {
-    if (eventData.type === "send-wasm-module") {
+  onmessage(event) {
+    if (event.type === "send-wasm-module") {
       // AudioNode has sent us a message containing the Wasm library to load into
       // our context as well as information about the audio device used for
       // recording.
-      init(WebAssembly.compile(eventData.wasmBytes)).then(() => {
-        this.detector = WasmPitchDetector.new(eventData.sampleRate);
+      init(WebAssembly.compile(event.wasmBytes)).then(() => {
+        this.port.postMessage({ type: 'wasm-module-loaded' });
       });
+    } else if (event.type === 'init-detector') {
+      const { sampleRate, numAudioSamplesPerAnalysis } = event;
+
+      // Store this because we use it later to detect when we have enough recorded
+      // audio samples for our first analysis.
+      this.numAudioSamplesPerAnalysis = numAudioSamplesPerAnalysis;
+
+      this.detector = WasmPitchDetector.new(sampleRate, numAudioSamplesPerAnalysis);
+
+      // Holds a buffer of audio sample values that we'll send to the Wasm module
+      // for analysis at regular intervals.
+      this.samples = new Array(numAudioSamplesPerAnalysis).fill(0);
+      this.totalSamples = 0;
     }
   };
 
   process(inputs, outputs) {
-    const BUFFER_LENGTH = 1024;
+    // inputs contains incoming audio samples for further processing. outputs
+    // contains the audio samples resulting from any processing performed by us.
+    // Here, we are performing analysis only to detect pitches so do not modify
+    // outputs.
 
-    // By default, the node has single input and output.
+    // inputs holds one or more "channels" of samples. For example, a microphone
+    // that records "in sterio" would provide two channels. For this simple app,
+    // we use assume either "mono" input or the "left" channel if microphone is
+    // sterio.
+
     const inputChannels = inputs[0];
+
+    // inputSamples holds an array of new samples to process.
     const inputSamples = inputChannels[0];
-    if (!inputSamples) return;
 
-    this.samples = [...this.samples, ...inputSamples];
-    this.samples = this.samples.slice(
-      Math.max(0, this.samples.length - BUFFER_LENGTH)
-    );
+    // In the AudioWorklet spec, process() is called whenever exactly 128 new
+    // audio samples have arrived. We simplify the logic for filling up the
+    // buffer by making an assumption that the analysis size is 128 samples or
+    // larger and is a power of 2.
+    if (this.totalSamples < this.numAudioSamplesPerAnalysis) {
+      for (const sampleValue of inputSamples) {
+        this.samples[this.totalSamples++] = sampleValue;
+      }
+    } else {
+      // Buffer is already full. We do not want the buffer to grow continually,
+      // so instead will "cycle" the samples through it so that it always
+      // holds the latest ordered samples of length equal to
+      // numAudioSamplesPerAnalysis.
 
-    if (this.samples.length >= 1024 && this.detector) {
+      // Shift the existing samples left by the length of new samples (128).
+      const numNewSamples = inputSamples.length;
+      const numExistingSamples = this.samples.length - numNewSamples;
+      for (let i = 0; i < numExistingSamples; i++) {
+        this.samples[i] = this.samples[i + numNewSamples];
+      }
+      // Add the new samples onto the end, into the 128-wide slot vacated by
+      // the previous copy.
+      for (let i = 0; i < numNewSamples; i++) {
+        this.samples[numExistingSamples + i] = inputSamples[i];
+      }
+      this.totalSamples += inputSamples.length;
+    }
+
+    // Once our buffer has enough samples, pass them to the Wasm pitch detector.
+    if (this.totalSamples >= this.numAudioSamplesPerAnalysis && this.detector) {
       const result = this.detector.detect_pitch(this.samples);
 
       if (result !== 0) {
@@ -44,6 +93,7 @@ class AudioProcessor extends AudioWorkletProcessor {
       }
     }
 
+    // Returning true tells Audio system to keep going.
     return true;
   }
 }
